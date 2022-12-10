21c3893f1fd3f1196e2d1c905e57a46e3619516b
Wzeun <volalovalist@gmail.com>
2022-12-10 22:41:26 +0200

    this is a all file

---
+++ MyProject/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyProject
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите дату:");
+            string Day = Console.ReadLine();
+            int DayConvertToInt = Convert.ToInt32(Day);
+
+            if (DayConvertToInt > 531){
+
+                Console.WriteLine("Введено не коректну дату");
+            }
+
+            Console.WriteLine("Введите месяц:");
+            string Month = Console.ReadLine();
+            int MonthConvertToInt = Convert.ToInt32(Month);
+
+            if (MonthConvertToInt > 12) {
+                Console.WriteLine("Введено некоректний місяць");
+            }
+
+            int NewYearDay = 31;
+            int NewYearMonth = 12;
+
+
+            Console.WriteLine($"Новий рік буде через {NewYearDay - DayConvertToInt} днів і {NewYearMonth - MonthConvertToInt} місяців. ");
+
+        }
+
+    }
+}
